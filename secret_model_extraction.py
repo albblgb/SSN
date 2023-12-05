46e4c8da9e6a39bf190d32c3a718a320cf5095e8
gbli <39871676+albblgb@users.noreply.github.com>
2023-12-05 16:06:17 +0800

    Update secret_model_extraction.py

--- secret_model_extraction.py
+++ secret_model_extraction.py
@@ -7,7 +7,7 @@ from torch.optim.lr_scheduler import MultiStepLR
 from tqdm import tqdm
 import logging
 
-from models.resnet18 import ResNet18
+from models.vgg import VGG
 from utils.dataset import load_datasets
 from utils.terminal import MetricMonitor
 from utils.logger import logger_info
@@ -16,14 +16,14 @@ from utils.proposed_mothod import secret_model_extraction, side_info_extraction,
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model_save_path = 'checkpoint/extracted_seceret_model/resnet18.pt'
+model_save_path = 'checkpoint/extracted_seceret_model/vgg.pt'
 mkdirs('checkpoint/extracted_seceret_model')
 mkdirs('results/extracted_secret_model')
 logger_name = 'extracted_secret_model'
-logger_info(logger_name, log_path=os.path.join('results', logger_name, 'resnet18.log'))
+logger_info(logger_name, log_path=os.path.join('results', logger_name, 'vgg.log'))
 logger = logging.getLogger(logger_name)
 logger.info('#'*50)
-logger.info('model: {:s}'.format('resnet18'))
+logger.info('model: {:s}'.format('vgg11'))
 logger.info('secret dataset: {:s}'.format('fashion-mnist'))
 
 
@@ -60,7 +60,7 @@ if __name__ == '__main__':
     lr = 1e-3
     bs = 128 # batch size
     num_classes = 10 # fashion-mnist
-    stego_model_path = 'checkpoint/stego_model/resnet18.pt' # pre-trained
+    stego_model_path = 'checkpoint/stego_model/vgg.pt' # pre-trained
     key = 10000
 
     logger.info('Loading secret datasets')
@@ -70,18 +70,19 @@ if __name__ == '__main__':
     stego_model = (torch.load(stego_model_path))
 
     logger.info('Extracting the B_stream and bn_running info form the stego model')
-    B_stream, bn_running_binary_ext = side_info_extraction(stego_model, key)
+    B_stream, bn_running_binary = side_info_extraction(stego_model, key)
 
     logger.info('Extracting the secret sub-model composed by selected filters from the stego model')
     secret_model = secret_model_extraction(stego_model, B_stream)
 
     logger.info('Recovering the running means and vars of bn layers')      
-    secret_model = bn_running_embedding(secret_model, bn_running_binary_ext)
+    secret_model = bn_running_embedding(secret_model, bn_running_binary)
     secret_model = secret_model.to(device)
 
     logger.info('Testing the extracted secret sub-network')
     criterion = nn.CrossEntropyLoss()
     test(secret_model, secret_test_loader, model_save_path)
+
 
 
     
